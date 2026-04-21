17505c0aedd33abf7372e18cd671479322f59a4d
kosandra3-debug <kosandra3@gmail.com>
2026-04-21 22:43:37 +0300

    2

--- PsychoPy_test_2.js
+++ PsychoPy_test_2.js
@@ -309,32 +309,38 @@ async function experimentInit() {
     depth: 0.0 
   });
   
-  // psychoJS._saveResults = 0;
-  // let filename = psychoJS._experiment._experimentName + '_' +
-  // psychoJS._experiment._datetime + '.csv';
-  // let dataObj = psychoJS._experiment._trialsData;
-  // let data = [Object.keys(dataObj[0])].concat(dataObj).map(it => {
-  // return Object.values(it).toString()
-  // }).join('\n')
-  // console.log('Saving data...');
-  // fetch('https://pipe.jspsych.org/api/data', {
-  //  method: 'POST',
-  //  headers: {
-  //  'Content-Type': 'application/json',
-  //  Accept: '*/*',
-  //  },
-  //  body: JSON.stringify({
-  //  experimentID: 'w4tfe',
-  //  filename: filename,
-  //  data: data,
-  //  }),
-  // }).then(response => response.json()).then(data => {
-  // console.log(data);
-  // quitPsychoJS();
-  // })
-  // // Create some handy timers
-  // globalClock = new util.Clock();  // to track the time since experiment started
-  // routineTimer = new util.CountdownTimer();  // to track time remaining of each (non-slip) routine
+  // Disable downloading results to browser
+  psychoJS._saveResults = 0;
+  // Generate filename for results
+  let filename = psychoJS._experiment._experimentName + '_' +
+  psychoJS._experiment._datetime + '.csv';
+  // Extract data object from experiment
+  let dataObj = psychoJS._experiment._trialsData;
+  // Convert data object to CSV
+  let data = [Object.keys(dataObj[0])].concat(dataObj).map(it => {
+  return Object.values(it).toString()
+  }).join('\n')
+  // Send data to OSF via DataPipe
+  console.log('Saving data...');
+  fetch('https://pipe.jspsych.org/api/data', {
+   method: 'POST',
+   headers: {
+   'Content-Type': 'application/json',
+   Accept: '*/*',
+   },
+   body: JSON.stringify({
+   experimentID: 'w4tfe', // * обновить, указав experiment ID из DATAPIPE на шаге 4.3 *
+   filename: filename,
+   data: data,
+   }),
+  }).then(response => response.json()).then(data => {
+  // Log response aud force experiment end
+  console.log(data);
+  quitPsychoJS();
+  })
+  // Create some handy timers
+  globalClock = new util.Clock();  // to track the time since experiment started
+  routineTimer = new util.CountdownTimer();  // to track time remaining of each (non-slip) routine
   
   return Scheduler.Event.NEXT;
 }
